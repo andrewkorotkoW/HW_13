ea0af0f17289995fa9a05c9f57149eea0d2d3761
andrewkorotkoW <123883439+andrewkorotkoW@users.noreply.github.com>
2023-02-28 01:58:40 +0300

    Add files via upload

---
+++ HW_13_3.py
@@ -0,0 +1,23 @@
+tickets = int(input('Введите количество билетов:'))
+
+ages = []
+
+for i in range(0, tickets):
+    input_value = int(input(f"Введите возраст участника № {i + 1}:\n"))
+    ages.append(input_value)
+
+    def prise(age):
+        if age < 18:
+            return 0
+        elif 18 <= age < 25:
+            return 990
+        else:
+            return 1390
+
+full_prise = sum(map(prise, ages))
+
+discount_prise = int(full_prise * 0.9)
+if tickets >= 3:
+    print("Стоимость билетов со скидкой: ", discount_prise, "rub.")
+else:
+    print("Стоимость всех билетов: ", full_prise, "rub.")
